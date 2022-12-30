c2185e2c86e8081aeaa2ccdb95e6fac0e4ab6d50
Bikram Thapa Magar <Bikram_ThapaMagar@uhaul.com>
2022-12-30 14:57:28 -0700

    add transaction date in income response

--- src/Perfi.Api/Responses/NewIncomeAddedResponse.cs
+++ src/Perfi.Api/Responses/NewIncomeAddedResponse.cs
@@ -8,7 +8,7 @@ namespace Perfi.Api.Responses
         public string Source { get; private set; }
         public MoneyResponse Amount { get; private set; }
         public IncomePaymentMethodResponse PaymentMethod { get; private set; }
-
+        public long TransactionUnixTimeMilliseconds { get; private set; }
         internal static NewIncomeAddedResponse From(IncomeDocument incomeDocument)
         {
             return new NewIncomeAddedResponse
@@ -16,7 +16,8 @@ namespace Perfi.Api.Responses
                 Id = incomeDocument.Id,
                 Source = incomeDocument.Source.Name,
                 Amount = MoneyResponse.From(incomeDocument.Amount),
-                PaymentMethod = IncomePaymentMethodResponse.From(incomeDocument)
+                PaymentMethod = IncomePaymentMethodResponse.From(incomeDocument),
+                TransactionUnixTimeMilliseconds = incomeDocument.TransactionDate.ToUnixTimeMilliseconds()
             };
         }
     }
