c7f7086853ced9c0138a2fda9ec2e95bfc812a93
Bikram Thapa Magar <Bikram_ThapaMagar@uhaul.com>
2022-12-30 09:25:40 -0700

    add summary category expense

---
+++ src/Perfi.Api/Responses/SummaryCategoryExpenseReportResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Perfi.Api.Responses
+{
+    public class SummaryCategoryExpenseReportResponse
+    {
+        public string SummaryCategoryCode { get; set; }
+        public string SummaryCategoryName { get; set; }
+        public MoneyResponse TotalExpenseAmount { get; set; }
+    }
+}
